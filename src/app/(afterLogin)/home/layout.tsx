d886f0724c26a5ebe60233e63511eaeed00a93a2
byjun8932 <byjun8932@gmail.com>
2023-12-20 12:58:53 +0900

    9.css module을 선택한 이유

--- src/app/(afterLogin)/home/layout.tsx
+++ src/app/(afterLogin)/home/layout.tsx
@@ -1,9 +1,7 @@
 export default async function HomeLayout({children}) {
     return(
         <div>
-            ==========홈레이아웃 시작========
             {children}
-            ==========홈레이아웃 끝========
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,17 @@ import styles from './page.module.css';
 import Link from 'next/link';
 import zlogo from '../../public/zlogo.png'
 
+// tailwind -> 호불호 너무 심하고, 가독성이 안좋다.
+// Styled Component -> Server Component 에서 SSR 문제가 있음
+// sass
+// css module -> 간단하게 가자
+// vanilla extract -> Windows 와 문제가 있음
+
 export default function Home(){
     return(
-        <div>
+        <>
             <div className={styles.left}>
-                <Image src={zlogo} alt="logo"/>
+                <Image src={zlogo} alt="logo" priority={true}/>
             </div>
             <div className={styles.right}>
                 <h1>지금 일어나고 있는 일</h1>
@@ -17,7 +23,6 @@ export default function Home(){
                 <h3>이미 트위터에 가입하셨나요?</h3>
                 <Link href='/login' className={styles.login}>로그인</Link>
             </div>
-        </div>
-
+        </>
     )
 }
