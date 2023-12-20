7567e422b7725007128412271b30051cc6532d82
byjun8932 <byjun8932@gmail.com>
2023-12-20 10:54:39 +0900

    8.template.tsx, Link, Image, redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
+import Link from 'next/link';
+import zlogo from '../../public/zlogo.png'
 
 export default function Home(){
     return(
-        <div>리액트 14 강좌의 시작 페이지 입니다.</div>
+        <div>
+            <div className={styles.left}>
+                <Image src={zlogo} alt="logo"/>
+            </div>
+            <div className={styles.right}>
+                <h1>지금 일어나고 있는 일</h1>
+                <h2>지금 가입하세요.</h2>
+                <Link href="/i/flow/signup" className={styles.signup}>계정 만들기</Link>
+                <h3>이미 트위터에 가입하셨나요?</h3>
+                <Link href='/login' className={styles.login}>로그인</Link>
+            </div>
+        </div>
+
     )
 }
